85d850a47d06faad0de4063c02dd7a1180a29b5a
Quackers026 <147959475+Quackers026@users.noreply.github.com>
2023-10-14 22:58:52 -0400

    Update A.py

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 9999 # assign numbers or strings to the end of these versions
+wacky_robot_name = "Jimbo the Himbo"
+adjective_1 = "repulsive"
+violent_verb = "immolate"
+tech_company = "Microsoft"
+dollar_amount = "$10"
+adjective_2 = "nauseating"
+noun_1 = "Ford F-150"
+large_store = "Dollar General"
